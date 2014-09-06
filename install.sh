8e45574db64ed8c93ba09e600197fdff07d07961
Eduardo San Martin Morote <eduardo.san-martin-morote@ensimag.grenoble-inp.fr>
2014-09-06 17:22:31 +0200

    indented functions ins install.sh

--- install.sh
+++ install.sh
@@ -29,31 +29,31 @@ BLUE="\e[94m"
 RESET="\e[m"
 
 good_msg() {
-local M
-[[ -n "$1" ]] && M="$1" || M=" [✔]"
-echo -e "${GREEN}${M}${RESET}"
+  local M
+  [[ -n "$1" ]] && M="$1" || M=" [✔]"
+  echo -e "${GREEN}${M}${RESET}"
 }
 
 bad_msg() {
-local M
-[[ -n "$1" ]] && M="$1" || M=" [✗]" # 🚸
-echo -e "${RED}${M}${RESET}"
+  local M
+  [[ -n "$1" ]] && M="$1" || M=" [✗]" # 🚸
+  echo -e "${RED}${M}${RESET}"
 }
 
 info_msg() {
-if [[ "$1" = "-n" ]]; then
-  echo -ne "${BLUE}${2}${RESET}"
-else
-  echo -e "${BLUE}${1}${RESET}"
-fi
+  if [[ "$1" = "-n" ]]; then
+    echo -ne "${BLUE}${2}${RESET}"
+  else
+    echo -e "${BLUE}${1}${RESET}"
+  fi
 }
 
 warning_msg() {
-if [[ "$1" = "-n" ]]; then
-  echo -ne "${YELLOW}${2}${RESET}"
-else
-  echo -e "${YELLOW}${1}${RESET}"
-fi
+  if [[ "$1" = "-n" ]]; then
+    echo -ne "${YELLOW}${2}${RESET}"
+  else
+    echo -e "${YELLOW}${1}${RESET}"
+  fi
 }
 
 ####### Verifications and backup #######
@@ -92,111 +92,111 @@ done
 
 # Install Homebrew only for OSX
 install_brew() {
-if [[ "$OSX" ]]; then
-  if [[ ! -x $(which brew) ]]; then
-    info_msg -n "Installing Homebrew(brew)..."
-    if ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)" ; then
+  if [[ "$OSX" ]]; then
+    if [[ ! -x $(which brew) ]]; then
+      info_msg -n "Installing Homebrew(brew)..."
+      if ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)" ; then
+        good_msg "done"
+      else
+        bad_msg "error"
+        exit 1
+      fi
+    fi
+  fi
+}
+
+install_git() {
+  if [[ ! -x $(which git) ]]; then
+    info_msg -n "Installing Git..."
+    if ${INSTALL} git; then
       good_msg "done"
     else
       bad_msg "error"
       exit 1
     fi
   fi
-fi
-}
-
-install_git() {
-if [[ ! -x $(which git) ]]; then
-  info_msg -n "Installing Git..."
-  if ${INSTALL} git; then
-    good_msg "done"
-  else
-    bad_msg "error"
-    exit 1
-  fi
-fi
 }
 
 # Install zsh, need git and brew
 install_zsh() {
-# Test to see if zshell is installed.  If it is:
-if [[ -x $(which zsh) ]]; then
-  # Clone my oh-my-zsh repository from GitHub only if it isn't already present
-  if [[ ! -d ${dir}/oh-my-zsh/ ]]; then
-    info_msg "Cloning oh-my-zsh"
-    if ! git clone https://github.com/robbyrussell/oh-my-zsh.git; then
-      bad_msg "error"
-      exit 1
-    fi
-  fi
-  # Set the default shell to zsh if it isn't currently set to zsh
-  if [[ ! "$SHELL" == $(which zsh) ]]; then
-    if [[ ! "$(grep "$(which zsh)" /etc/shells)" ]]; then
-      if [[ $(uname) == 'Linux' || $(uname) == 'Darwin' ]]; then
-        #which zsh | sudo tee -a /etc/shells
-        chsh -s "$(which zsh)" || warning_msg "Manually change it with chsh -s $(which zsh)"
+  # Test to see if zshell is installed.  If it is:
+  if [[ -x $(which zsh) ]]; then
+    # Clone my oh-my-zsh repository from GitHub only if it isn't already present
+    if [[ ! -d ${dir}/oh-my-zsh/ ]]; then
+      info_msg "Cloning oh-my-zsh"
+      if ! git clone https://github.com/robbyrussell/oh-my-zsh.git; then
+        bad_msg "error"
+        exit 1
       fi
     fi
-  fi
+    # Set the default shell to zsh if it isn't currently set to zsh
+    if [[ ! "$SHELL" == $(which zsh) ]]; then
+      if [[ ! "$(grep "$(which zsh)" /etc/shells)" ]]; then
+        if [[ $(uname) == 'Linux' || $(uname) == 'Darwin' ]]; then
+          #which zsh | sudo tee -a /etc/shells
+          chsh -s "$(which zsh)" || warning_msg "Manually change it with chsh -s $(which zsh)"
+        fi
+      fi
+    fi
 
-  # Install posva zsh theme
-  zsh_theme="oh-my-zsh/themes/posva.zsh-theme"
-  if [ ! -f $zsh_theme ]; then
-    info_msg -n "Installing zsh theme..."
-    wget -q https://raw.github.com/posva/oh-my-zsh/6e611f2f45320eef572d13fc3c57391fd0beedb3/themes/posva.zsh-theme -O $zsh_theme && good_msg "done" || bad_msg "error"
+    # Install posva zsh theme
+    zsh_theme="oh-my-zsh/themes/posva.zsh-theme"
+    if [ ! -f $zsh_theme ]; then
+      info_msg -n "Installing zsh theme..."
+      wget -q https://raw.github.com/posva/oh-my-zsh/6e611f2f45320eef572d13fc3c57391fd0beedb3/themes/posva.zsh-theme -O $zsh_theme && good_msg "done" || bad_msg "error"
+    fi
+  else
+    info_msg "Installig zsh"
+    ${INSTALL} zsh || exit 1
+    # install oh-my-zsh if installation was successful
+    install_zsh
   fi
-else
-  info_msg "Installig zsh"
-  ${INSTALL} zsh || exit 1
-  # install oh-my-zsh if installation was successful
-  install_zsh
-fi
 }
 
 # Install some programs I cannot live without
 install_more() {
-if [[ ! -x $(which source-highlight) ]]; then
-  info_msg "Installing source-highlight"
-  ${INSTALL} source-highlight && good_msg "done" || bad_msg "Error installing source-highlight"
-fi
+  if [[ ! -x $(which source-highlight) ]]; then
+    info_msg "Installing source-highlight"
+    ${INSTALL} source-highlight && good_msg "done" || bad_msg "Error installing source-highlight"
+  fi
 }
 
 # Install vim and plugins
 install_vim() {
-if [[ ! -x $(which vim) ]]; then
-  if [[ "$OSX" ]]; then
-    brew install vim --with-lua
-  else
-    if [[ ! -x $(which hg) ]]; then
-      info_msg "Installing hg(Mercurial)"
-      ${INSTALL} mercurial || exit 1
+  if [[ ! -x $(which vim) ]]; then
+    if [[ "$OSX" ]]; then
+      brew install vim --with-lua
+    else
+      if [[ ! -x $(which hg) ]]; then
+        info_msg "Installing hg(Mercurial)"
+        ${INSTALL} mercurial || exit 1
+      fi
+      if [[ ! -d "vim_src" ]]; then
+        info_msg "Cloning vim repo"
+        hg clone https://vim.googlecode.com/hg/ vim_src || exit 1
+      fi
+      cd vim_src
+      info_msg "Installing Vim with python and ruby support"
+      ./configure --prefix=/usr/local/ --enable-rubyinterp --enable-pythoninterp --enable-luainterp --with-features=huge || exit 1
+      make || exit 1
+      sudo make install || exit 1
     fi
-    if [[ ! -d "vim_src" ]]; then
-      info_msg "Cloning vim repo"
-      hg clone https://vim.googlecode.com/hg/ vim_src || exit 1
+  fi
+
+  # backup dir
+  info_msg -n "Creating backup vim directory..."
+  mkdir -p vim/backup && good_msg "done" || warning_msg "Manually create the dir ~/.vim/backup"
+
+  # Plugins
+  if [[ ! -d ${dir}/vim/bundle/Vundle.vim/ ]]; then
+    info_msg "Installing Vundle"
+    if ! git clone https://github.com/gmarik/Vundle.vim.git vim/bundle/Vundle.vim ; then
+      bad_msg "Error doing git clone..."
+      exit 1
     fi
-    cd vim_src
-    info_msg "Installing Vim with python and ruby support"
-    ./configure --prefix=/usr/local/ --enable-rubyinterp --enable-pythoninterp --enable-luainterp --with-features=huge || exit 1
-    make || exit 1
-    sudo make install || exit 1
   fi
-fi
 
-# backup dir
-info_msg -n "Creating backup vim directory..."
-mkdir -p vim/backup && good_msg "done" || warning_msg "Manually create the dir ~/.vim/backup"
-
-# Plugins
-if [[ ! -d ${dir}/vim/bundle/Vundle.vim/ ]]; then
-  info_msg "Installing Vundle"
-  if ! git clone https://github.com/gmarik/Vundle.vim.git vim/bundle/Vundle.vim ; then
-    bad_msg "Error doing git clone..."
-    exit 1
-  fi
-fi
-
-#vim -Nu "$dir/vim-plugins.vim" +PluginInstall! +qall
+  #vim -Nu "$dir/vim-plugins.vim" +PluginInstall! +qall
 }
 
 install_font() {
@@ -210,18 +210,18 @@ install_font() {
 }
 
 install_powerfonts() {
-info_msg -n "Copying powerline-fonts..."
-for f in $dir/powerline-fonts/*; do
-  if ! install_font "$f"; then
-    bad_msg "error copying ${f}..."
-    return 1
+  info_msg -n "Copying powerline-fonts..."
+  for f in $dir/powerline-fonts/*; do
+    if ! install_font "$f"; then
+      bad_msg "error copying ${f}..."
+      return 1
+    fi
+  done
+  good_msg "done"
+  if [[ -z "$OSX" ]]; then
+    info_msg "Updating font cache..."
+    fc-cache -fv || bad_msg "Error"
   fi
-done
-good_msg "done"
-if [[ -z "$OSX" ]]; then
-  info_msg "Updating font cache..."
-  fc-cache -fv || bad_msg "Error"
-fi
 }
 
 install_powerline() {
