d84b193c30cf000fc1eeb7622823e4ed6ec1f957
Eduardo San Martin Morote <posva13@gmail.com>
2024-11-15 10:46:00 +0100

    add debugging to neovim

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,3 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.winbar = "%=%m %f"
+
+vim.opt.conceallevel = 0
+vim.opt.cmdheight = 0
+
+vim.g.root_spec = { "cwd" }
