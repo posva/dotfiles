16896577455bd76049ec9bcc48a0f53a1d394956
Eduardo San Martin Morote <posva13@gmail.com>
2024-12-23 10:02:04 +0100

    neotest updates

--- nvim/lua/plugins/test.lua
+++ nvim/lua/plugins/test.lua
@@ -2,12 +2,15 @@ return {
   {
     "nvim-neotest/neotest",
     dependencies = {
+      -- Adapters
       -- "nvim-neotest/neotest-jest",
       "marilari88/neotest-vitest",
+      "thenbe/neotest-playwright",
     },
     opts = function(_, opts)
       -- table.insert(opts.adapters, require("neotest-jest"))
-      table.insert(opts.adapters, require("neotest-vitest"))
+      -- table.insert(opts.adapters, require("neotest-vitest"))
+      opts.adapters["neotest-vitest"] = {}
     end,
   },
 }
