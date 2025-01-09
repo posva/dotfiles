e68da51e035f7b014dde7b3a79b54c4e942d77f0
Eduardo San Martin Morote <posva13@gmail.com>
2025-01-09 11:08:24 +0100

    leap

---
+++ .config/nvim/lua/plugins/leap.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "ggandor/leap.nvim",
+    keys = {
+      { "s", mode = { "n", "x", "o" }, desc = "Leap Forward to" },
+      { "S", mode = { "n", "x", "o" }, desc = "Leap Backward to" },
+    },
+  },
+  {
+    "echasnovski/mini.surround",
+    opts = {
+      mappings = {
+        add = "gsa", -- Add surrounding in Normal and Visual modes
+        delete = "gsd", -- Delete surrounding
+        find = "gsf", -- Find surrounding (to the right)
+        find_left = "gsF", -- Find surrounding (to the left)
+        highlight = "gsh", -- Highlight surrounding
+        replace = "gsr", -- Replace surrounding
+        update_n_lines = "gsn", -- Update `n_lines`
+      },
+    },
+    keys = {
+      { "gs", "", desc = "+surround" },
+    },
+  },
+}
