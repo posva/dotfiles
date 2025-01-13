6a2fd98a4bc36780a8ecb065a7cac39f1584a9de
Eduardo San Martin Morote <posva13@gmail.com>
2025-01-13 11:30:58 +0100

    wip avante

---
+++ .config/nvim/lua/plugins/ai.lua
@@ -0,0 +1,64 @@
+return {
+  enabled = false,
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  lazy = false,
+  version = false, -- set this to "*" if you want to always pull the latest change, false to update on release
+  opts = {
+    ---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
+    provider = "claude", -- Recommend using Claude
+    auto_suggestions_provider = "copilot", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
+    claude = {
+      endpoint = "https://api.anthropic.com",
+      model = "claude-3-5-sonnet-20241022",
+      temperature = 0,
+      max_tokens = 4096,
+    },
+
+    windows = {
+      file_selector = {
+        --- @alias FileSelectorProvider "native" | "fzf" | "telescope" | string
+        provider = "fzf",
+        -- Options override for custom providers
+        provider_opts = {},
+      },
+    },
+  },
+  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  build = "make",
+  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+  dependencies = {
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    --- The below dependencies are optional,
+    "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    "zbirenbaum/copilot.lua", -- for providers='copilot'
+    {
+      -- support for image pasting
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- recommended settings
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          -- required for Windows users
+          use_absolute_path = true,
+        },
+      },
+    },
+    {
+      -- Make sure to set this up properly if you have lazy=true
+      "MeanderingProgrammer/render-markdown.nvim",
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
+  },
+}
---
+++ .config/nvim/lua/plugins/completions.lua
@@ -0,0 +1,35 @@
+return {
+  -- NOTE: decomment if ai.lua with avante is enabled
+  -- "saghen/blink.cmp",
+  -- sources = {
+  --   providers = {
+  --     avante_commands = {
+  --       name = "avante_commands",
+  --       module = "blink.compat.source",
+  --       score_offset = 90, -- show at a higher priority than lsp
+  --       opts = {},
+  --     },
+  --     avante_files = {
+  --       name = "avante_commands",
+  --       module = "blink.compat.source",
+  --       score_offset = 100, -- show at a higher priority than lsp
+  --       opts = {},
+  --     },
+  --     avante_mentions = {
+  --       name = "avante_mentions",
+  --       module = "blink.compat.source",
+  --       score_offset = 1000, -- show at a higher priority than lsp
+  --       opts = {},
+  --     },
+  --   },
+  --   -- adding any nvim-cmp sources here will enable them
+  --   -- with blink.compat
+  --   compat = {
+  --     "avante_commands",
+  --     "avante_mentions",
+  --     "avante_files",
+  --   },
+  --   default = { "lsp", "path", "snippets", "buffer" },
+  --   cmdline = {},
+  -- },
+}
