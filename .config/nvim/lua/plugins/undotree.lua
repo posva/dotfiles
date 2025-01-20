423eeefbcc50cc31de8e1257181ac68f319ce819
Eduardo San Martin Morote <posva13@gmail.com>
2025-01-20 09:59:29 +0100

    undotree description

--- .config/nvim/lua/plugins/undotree.lua
+++ .config/nvim/lua/plugins/undotree.lua
@@ -5,6 +5,10 @@ return {
   config = true,
   keys = {
     -- load the plugin only when using it's keybinding:
-    { "<leader>U", "<cmd>lua require('undotree').setup()<cr><cmd>lua require('undotree').toggle()<cr>" },
+    {
+      "<leader>U",
+      "<cmd>lua require('undotree').setup()<cr><cmd>lua require('undotree').toggle()<cr>",
+      desc = "Undotree",
+    },
   },
 }
