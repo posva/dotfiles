9ce9983701c4f9bf81ccd6c8da05c5634fc44f3e
Eduardo San Martin Morote <posva13@gmail.com>
2025-02-13 12:59:21 +0100

    fix vscode nvim

--- .config/nvim/lua/plugins/treesj.lua
+++ .config/nvim/lua/plugins/treesj.lua
@@ -1,6 +1,8 @@
 return {
   "Wansmer/treesj",
   keys = { "<space>jt" },
+  -- disable if running in vscode
+  vscode = true,
   dependencies = { "nvim-treesitter/nvim-treesitter" }, -- if you install parsers with `nvim-treesitter`
   config = function()
     require("treesj").setup({
