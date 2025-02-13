1ac34ea9e0b2422bcf97f72ab9417fcc4ab9bc08
Eduardo San Martin Morote <posva13@gmail.com>
2025-02-13 12:59:33 +0100

    default configs debug

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -17,6 +17,35 @@ return {
         desc = "Step Over",
       },
     },
+    opts = function()
+      local dap = require("dap")
+      for _, language in ipairs({ "typescript", "javascript" }) do
+        dap.configurations[language] = {
+          {
+            type = "pwa-node",
+            request = "launch",
+            name = "Launch file",
+            program = "${file}",
+            cwd = "${workspaceFolder}",
+          },
+          {
+            type = "pwa-node",
+            request = "attach",
+            name = "Attach",
+            processId = require("dap.utils").pick_process,
+            cwd = "${workspaceFolder}",
+          },
+          {
+            type = "pwa-chrome",
+            request = "launch",
+            name = 'Start Chrome with "localhost"',
+            url = "http://localhost:3000",
+            webRoot = "${workspaceFolder}",
+            userDataDir = "${workspaceFolder}/.vscode/vscode-chrome-debug-userdatadir",
+          },
+        }
+      end
+    end,
   },
   {
     "theHamsta/nvim-dap-virtual-text",
