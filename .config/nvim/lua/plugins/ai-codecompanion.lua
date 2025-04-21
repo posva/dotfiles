49aaf22f71a1577b343a4262ddb87a60923fbc4c
Eduardo San Martin Morote <posva13@gmail.com>
2025-04-21 18:29:46 +0200

    disable half working ai stuff

---
+++ .config/nvim/lua/plugins/ai-codecompanion.lua
@@ -0,0 +1,9 @@
+return {
+  enabled = false,
+  "olimorris/codecompanion.nvim",
+  opts = {},
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+}
--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -1,5 +1,5 @@
 return {
-  enabled = true,
+  enabled = false,
   "yetone/avante.nvim",
   event = "VeryLazy",
   lazy = false,
