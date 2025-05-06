714011497262780bea1b0cb769449c21e8ed3ac9
Eduardo San Martin Morote <posva13@gmail.com>
2025-05-06 08:24:39 +0200

    up vim

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,16 +1,33 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = function()
-    local keys = require("lazyvim.plugins.lsp.keymaps").get()
-    -- disable a keymap
-    keys[#keys + 1] = { "K", false }
-    -- change a keymap
-    keys[#keys + 1] = {
-      "gh",
-      function()
-        return vim.lsp.buf.hover()
-      end,
-      desc = "Hover",
-    }
-  end,
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    opts = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      -- disable a keymap
+      keys[#keys + 1] = { "K", false }
+      -- change a keymap
+      keys[#keys + 1] = {
+        "gh",
+        function()
+          return vim.lsp.buf.hover()
+        end,
+        desc = "Hover",
+      }
+    end,
+  },
 }
