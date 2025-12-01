1c3599678e10b081b54dc7049c59cee07635b2b2
Eduardo San Martin Morote <posva13@gmail.com>
2025-12-01 10:31:18 +0100

    up vim

--- .config/nvim/lua/plugins/leap.lua
+++ .config/nvim/lua/plugins/leap.lua
@@ -7,7 +7,7 @@ return {
     },
   },
   {
-    "echasnovski/mini.surround",
+    "nvim-mini/mini.surround",
     opts = {
       mappings = {
         add = "gsa", -- Add surrounding in Normal and Visual modes
