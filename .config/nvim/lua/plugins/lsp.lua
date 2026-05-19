bd99ac20ab1c7d3399fde6d1065f5b8d6e224c66
Eduardo San Martin Morote <posva13@gmail.com>
2026-05-19 16:08:34 +0200

    feat(nvim): add <leader>cu to remove unused (vtsls)

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -2,15 +2,24 @@ return {
   {
     "neovim/nvim-lspconfig",
     lazy = false,
-    opts = {
-      servers = {
-        ["*"] = {
-          keys = {
-            { "gh", function() return vim.lsp.buf.hover() end, desc = "Hover" },
-          },
-        },
-      },
-    },
+    opts = function(_, opts)
+      opts.servers = opts.servers or {}
+      opts.servers["*"] = opts.servers["*"] or {}
+      opts.servers["*"].keys = opts.servers["*"].keys or {}
+      table.insert(opts.servers["*"].keys, {
+        "gh",
+        function() return vim.lsp.buf.hover() end,
+        desc = "Hover",
+      })
+
+      opts.servers.vtsls = opts.servers.vtsls or {}
+      opts.servers.vtsls.keys = opts.servers.vtsls.keys or {}
+      table.insert(opts.servers.vtsls.keys, {
+        "<leader>cu",
+        LazyVim.lsp.action["source.removeUnused.ts"],
+        desc = "Remove unused",
+      })
+    end,
   },
 
   {
