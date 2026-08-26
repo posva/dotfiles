176d658c2c520b2d28b9020b57b34338b16810ac
Eduardo San Martin Morote <posva13@gmail.com>
2026-08-26 15:20:55 +0200

    install

--- install.sh
+++ install.sh
@@ -320,7 +320,7 @@ install_node() {
 }
 
 install_node_globals_with_mise() {
-  mise use -g npm:@antfu/ni npm:fkill-cli
+  mise use -g npm:@antfu/ni npm:fkill-cli npm:agent-browser
 }
 
 install_mise_completions() {
